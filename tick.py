57cef958ca6f769f8e6f3e93e98d2747315fdae6
matt <AF7TI@users.noreply.github.com>
2018-12-05 07:15:44 -0800

    Update tick.py

--- tick.py
+++ tick.py
@@ -1,25 +1,39 @@
 import subprocess
 import time
 import os
+import sys
+import logging
+import psycopg2
 
 cwd = os.getcwd()
 
-import psycopg2
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(threadName)-12.12s] [%(levelname)-5.5s]  %(message)s",
+    handlers=[
+        #logging.FileHandler("{0}/{1}.log".format(logPath, fileName)),
+        logging.StreamHandler(sys.stdout)
+    ])
+
+logger = logging.getLogger()
 
 try:
     con = psycopg2.connect("dbname='postgres' user='postgres' host='localhost' password='mysecretpassword'")
 except:
-    print ("Unable to connect to the database")
+    logger.error("Unable to connect to the database")
 
 cursor = con.cursor()
 
 try:
-    cursor.execute(open("test.sql", "r").read())
+    cursor.execute(open("dbsetup.sql", "r").read())
 except:
-    print ("Unable to execute database cursor")
+    logger.error("Unable to execute database cursor")
+
+cursor.close()
+con.close()
 
 starttime=time.time()
 while True:
-  print ("tick")
+  logger.info("start tick")
   subprocess.call(['python', cwd + '/tread.py', '1', '4'])
   time.sleep(300.0 - ((time.time() - starttime) % 300.0))
