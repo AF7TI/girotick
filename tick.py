d07f88b0c6c12772577897c593de0afddcc4a670
matt <AF7TI@users.noreply.github.com>
2019-02-08 07:30:05 -0800

    Update tick.py
    get db connection from environment

--- tick.py
+++ tick.py
@@ -5,11 +5,12 @@ import sys
 import logging
 import psycopg2
 
-DB_NAME='postgres'
-DB_USER='postgres'
-DB_HOST='localhost'
-DB_PASSWORD='mysecretpassword'
 DB_TIMEOUT=5000 #postgres timeout
+
+DB_NAME=os.getenv("DB_NAME")
+DB_USER=os.getenv("DB_USER")
+DB_HOST=os.getenv("DB_HOST")
+DB_PASSWORD=os.getenv("DB_PASSWORD")
 
 cwd = os.getcwd()
 
